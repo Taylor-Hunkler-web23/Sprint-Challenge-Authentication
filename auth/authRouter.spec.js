a560c4c101984c8bedf41ecdfd5f655e4f950337
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-15 12:21:46 -0500

    Created tests for login route checking for body type and body message

--- auth/authRouter.spec.js
+++ auth/authRouter.spec.js
@@ -2,45 +2,47 @@ const request = require('supertest'); //bring in supertest
 
 const server = require('../api/server.js') //bring in server
 
-const db = require ("../database/dbConfig.js")
+const db = require("../database/dbConfig.js")
 
-describe('register()', function () {
+describe('POST register()', function () {
 
-    
     beforeEach(async () => {
         await db('users').truncate();
-
     })
 
-
-
     it('should respond with status 200', async function () {
-
-   
-
         await request(server)
             .post("/api/auth/register")
-            .send({ username: "Jeep", password: "wrangler"})
+            .send({ username: "Jeep", password: "wrangler" })
             .then(res => {
                 expect(res.status).toBe(201)
             })
-
-
-
-
     });
-
-
-
 })
 
-describe('get', function () {
+describe('POST register', function () {
     describe('post /', function () {
         it('should return json formated response', function () {
 
-            return request(server)  
-                .post('/api/auth/register')  
-                .send({ username: "Jeep", password: "wrangler"})  
+            return request(server)
+                .post('/api/auth/register')
+                .send({ username: "Jeep", password: "wrangler" })
+                .then(res => {
+
+                    expect(res.type).toMatch(/json/i);
+                })
+        })
+    })
+})
+
+
+describe('POST login', function () {
+    describe('post /', function () {
+        it('should return json formated response', function () {
+
+            return request(server)
+                .post('/api/auth/login')
+                .send({ username: "Jeep", password: "wrangler" })
                 .then(res => {
 
                     expect(res.type).toMatch(/json/i); 
@@ -50,3 +52,41 @@ describe('get', function () {
 })
 
 
+
+
+// describe('POST login()', function () {
+//     beforeEach(async () => {
+//         await db('users').truncate();
+
+//     })
+//     it('should respond with status 200', async function () {
+
+//         return request(server)
+//         .post('/api/auth/register')
+//         .send({ username: "Jeep", password: "wrangler" })
+
+//         await request(server)
+//             .post("/api/auth/login")
+//             .send({ username: "Jeep", password: "wrangler" })
+//             .then(res => {
+//                 expect(res.status).toBe(200)
+//             })
+
+//     });
+// })
+
+
+describe('POST login', function () {
+    describe('post /', function () {
+        it('should return json formated response', function () {
+
+            return request(server)
+                .post('/api/auth/login')
+                .send({ username: "Jeep", password: "wranler" })
+                .then(res => {
+
+                    expect(res.body).toEqual({message:'Invalid Credentials'});
+                })
+        })
+    })
+})
